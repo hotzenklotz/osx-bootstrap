85661ec5b85b61b343f42b4b351112e61e745749
Tom Herold <tom.herold@scalableminds.com>
2019-06-02 09:31:11 +0200

    added bash-it

---
+++ .bash_profile
@@ -0,0 +1,15 @@
+source $HOME/.aliases
+
+# Path to the bash it configuration
+export BASH_IT="/Users/therold/.bash_it"
+
+# Lock and Load a custom theme file.
+# Leave empty to disable theming.
+# location /.bash_it/themes/
+export BASH_IT_THEME='clean'
+
+# Default Editor
+export EDITOR=subl
+
+# Load Bash It
+source "$BASH_IT"/bash_it.sh
--- bootstrap.sh
+++ bootstrap.sh
@@ -52,10 +52,9 @@ PACKAGES=(
     awscli
     cmake
     colordiff
-    docker
+    fasd
     ffmpeg
     findutils
-    flow
     gettext
     git
     grep
@@ -75,7 +74,6 @@ PACKAGES=(
     watch
     wget
     youtube-dl
-    z
 )
 
 echo "Installing packages..."
@@ -93,11 +91,12 @@ CASKS=(
     caffeine
     cncjs
     cyberduck
+    dopcker
     dropbox
     firefox
     gifox
     gimp
-    github-desktop
+    github
     google-backup-and-sync
     google-chrome
     iterm2
@@ -110,7 +109,7 @@ CASKS=(
     spectacle
     spotify
     steam
-    sublime-text-dev
+    sublime-text
     the-unarchiver
     ultimaker-cura
     viber
@@ -172,63 +171,24 @@ defaults write com.apple.dock tilesize -int 36
 # Pin dock to right side of screen
 defaults write com.apple.Dock orientation -string "right"
 
-
+# Ipen "Save" dialog expanded
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode -bool true
+defaults write NSGlobalDomain NSNavPanelExpandedStateForSaveMode2 -bool true
 
 killall Dock > /dev/null 2>&1
 killall Finder > /dev/null 2>&1
-
-echo "Setup Terminal aliases"
-
-# Easier navigation: .., ..., ...., .....
-alias ..="cd .."
-alias ...="cd ../.."
-alias ....="cd ../../.."
-alias .....="cd ../../../.."
-
-# Shortcuts
-alias d="cd ~/Desktop"
-alias dl="cd ~/Downloads"
-alias g="git"
-alias s="subl ."
-
-#git
-alias ga='git add'
-alias gall='git add .'
-alias gs='git status'
-alias gp='git push'
-alias gc='git commit -v'
-alias gco='git checkout'
-
-# Detect which `ls` flavor is in use
-if ls --color > /dev/null 2>&1; then # GNU `ls`
-    colorflag="--color"
-else # OS X `ls`
-    colorflag="-G"
-fi
-
-# List all files colorized in long format
-alias l="ls -l ${colorflag}"
-
-# List all files colorized in long format, including dot files
-alias la="ls -la ${colorflag}"
-
-# List only directories
-alias lsd='ls -l ${colorflag} | grep "^d"'
-
-# Always use color output for `ls`
-alias ls="command ls ${colorflag}"
-export LS_COLORS='no=00:fi=00:di=01;34:ln=01;36:pi=40;33:so=01;35:do=01;35:bd=40;33;01:cd=40;33;01:or=40;31;01:ex=01;32:*.tar=01;31:*.tgz=01;31:*.arj=01;31:*.taz=01;31:*.lzh=01;31:*.zip=01;31:*.z=01;31:*.Z=01;31:*.gz=01;31:*.bz2=01;31:*.deb=01;31:*.rpm=01;31:*.jar=01;31:*.jpg=01;35:*.jpeg=01;35:*.gif=01;35:*.bmp=01;35:*.pbm=01;35:*.pgm=01;35:*.ppm=01;35:*.tga=01;35:*.xbm=01;35:*.xpm=01;35:*.tif=01;35:*.tiff=01;35:*.png=01;35:*.mov=01;35:*.mpg=01;35:*.mpeg=01;35:*.avi=01;35:*.fli=01;35:*.gl=01;35:*.dl=01;35:*.xcf=01;35:*.xwd=01;35:*.ogg=01;35:*.mp3=01;35:*.wav=01;35:'
-
-# Enable aliases to be sudo’ed
-alias sudo='sudo '
-
-# Merge PDF files
-# Usage: `mergepdf -o output.pdf input{1,2,3}.pdf`
-alias mergepdf='/System/Library/Automator/Combine\ PDF\ Pages.action/Contents/Resources/join.py'
 
 echo "Copying Dotfiles"
 cp .inputrc ~
 cp .gitignore ~
 cp .gitconfig ~
+cp .aliases ~
+cp .bash_profile ~
+
+source ~/.bash_profile
+
+echo "Install bash-it"
+git clone --depth=1 https://github.com/Bash-it/bash-it.git ~/.bash_it
+~/.bash_it/install.sh --interactive #--silent
 
 echo "Bootstrapping complete"
